6dab769244315764aa5f44c8d015b9c3df020254
cascodenotch <catalinaarciniegassaenz@gmail.com>
2024-11-12 17:17:33 +0100

    Ajustes retos 1-3

--- crearpersona.js
+++ crearpersona.js
@@ -22,12 +22,27 @@ rl.question('¿Cuál es tu nombre? ', (name) => {
         person.age = age;
 
             fs.writeFile('crearpersona.json', JSON.stringify(person), (err) => {
+
+                if (err){
+                    console.log("No se ha podido guardar el fichero")
+                }
+                else{
+                    console.log("Se ha creado el fichero");
+                }
        
                 fs.readFile('crearpersona.json', 'utf8', (err, data) => {
-                const parsedPerson = JSON.parse(data);
-                console.log(parsedPerson);
 
-                rl.close();
+                    if (err){
+                        console.log("No se ha podido leer el fichero")
+                    }
+                    else{
+                        console.log("Se ha leido el fichero");
+                    }
+
+                    const parsedPerson = JSON.parse(data);
+                    console.log(parsedPerson);
+
+                    rl.close();
               })
             })
         })
--- person.js
+++ person.js
@@ -7,8 +7,23 @@ let person = {
 }
 
 fs.writeFile('person.json', JSON.stringify(person), (err) => {
+
+    if (err){
+        console.log("No se ha podido guardar el fichero")
+    }
+    else{
+        console.log("Se ha creado el fichero");
+    }
    
       fs.readFile('person.json', 'utf8', (err, data) => {
+
+        if (err){
+            console.log("No se ha podido leer el fichero")
+        }
+        else{
+            console.log("Se ha leido el fichero");
+        }
+
           const parsedPerson = JSON.parse(data);
           console.log(parsedPerson);
       });
