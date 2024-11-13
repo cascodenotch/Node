6eb49d42a8381158e225a7d628ef56b3e58c13ff
cascodenotch <catalinaarciniegassaenz@gmail.com>
2024-11-13 11:37:38 +0100

    Subir dos archivos nuevos

---
+++ promesas/writeAndReadPromesa.js
@@ -0,0 +1,48 @@
+const fs = require('fs/promises');
+
+let person1 = {
+    name: "Catalina",
+    surname:"Arciniegas",
+    age:32,
+}
+
+let person2 = {
+    name: "Paula",
+    surname:"Muñoz",
+    age:22,
+}
+
+// Con then y catch 
+
+// fs.writeFile ("person.json", JSON.stringify(person1))
+// .then (()=>{
+//     console.log("Fichero creado");
+//     return fs.readFile ("person.json","utf-8");
+// })
+// .then ((dato) => {
+//     console.log ("Fichero leido");
+//     console.log (dato);
+// })
+// .catch ((error)=>{
+//     console.log ("Hay un error");
+//     console.log (error);
+// }
+// )
+
+// Con async
+
+async function writeAndRead(){
+    try{
+        await fs.writeFile ("person.json", JSON.stringify(person2));
+        console.log ("Se ha creado el fichero");
+        const dato = await fs.readFile ("person.json", "utf-8");
+        console.log ("Se ha leido el fichero");
+        console.log (dato);
+    }
+    catch (error){
+        console.log("Hay un error");
+    }
+
+}
+
+writeAndRead();
