b0a6b3340cd6b2b2a0e1cc148beac47114c9f05c
cascodenotch <catalinaarciniegassaenz@gmail.com>
2024-11-18 16:49:22 +0100

    Ajustes en funciones

--- servidor-express/src/controller/arrayController.js
+++ servidor-express/src/controller/arrayController.js
@@ -4,6 +4,9 @@ let arrayBooks = [];
 let bookInstance = null; 
 
 function getAllBooks(request, response){
+
+    console.log("end point funciona");
+
     let respuesta;
     if (arrayBooks.length > 0) 
         respuesta = { error: false, codigo: 200, data: arrayBooks }; 
@@ -14,6 +17,9 @@ function getAllBooks(request, response){
 };
 
 function addBook(request, response){   
+
+    console.log("end point funciona");
+
     let respuesta; 
     let bookId = request.body.id_book;
     let bookExists = false;  
@@ -25,7 +31,7 @@ function addBook(request, response){
     });
 
     if (bookExists) {
-        respuesta = { error: true, codigo: 404, mensaje: 'Libro ya existe' };
+        respuesta = { error: true, codigo: 400, mensaje: 'Libro ya existe' };
     } 
     else { bookInstance = new Book(
             request.body.id_book, 
@@ -38,17 +44,19 @@ function addBook(request, response){
         
         arrayBooks.push(bookInstance);
         console.log('Nuevo libro:', bookInstance);
-        respuesta = { error: false, codigo: 200, mensaje: 'Libro añadido', data: arrayBooks };
+        respuesta = { error: false, codigo: 201, mensaje: 'Libro añadido', data: arrayBooks };
     }
 
     response.send(respuesta); 
 }
 
 function updateBook(request, response) {
+
+    console.log("end point funciona");
+
     let respuesta;
     let bookId = request.body.id_book;
     let bookExists = false;
-    console.log(request.body);
 
     arrayBooks.forEach(book => {
         if (book.id_book === bookId) {
@@ -60,59 +68,46 @@ function updateBook(request, response) {
             book.price = request.body.price;
             book.photo = request.body.photo;
 
-            respuesta = { error: false, codigo: 200, mensaje: 'Libro actualizado', data: book 
-            };
+            respuesta = { error: false, codigo: 200, mensaje: 'Libro actualizado', data: book};
         }
     });
 
     if (!bookExists) {
-        respuesta = { error: true, codigo: 404,mensaje: 'No existe ese Id' 
-        };
+        respuesta = { error: true, codigo: 404,mensaje: 'No existe ese Id'};
     }
     response.send(respuesta);
 }
 
 function deleteBook (request, response){
+
+    console.log("end point funciona");
+   
     let respuesta;
     let bookId = request.body.id_book;
-    let bookExists = false;  
-
-    arrayBooks.forEach(book => {
-        if (book.id_book === bookId) {
-            bookExists = true;
-        }
-    });
-    
-    if (bookExists){
     let arrayFiltrado = arrayBooks.filter(book => (book.id_book != bookId))
-    arrayBooks = arrayFiltrado;
-    respuesta = {error: false,codigo: 200, mensaje: 'Libro eliminado', data:arrayBooks};
-    }
 
-    else {
-        respuesta = {
-            error: true,
-            codigo: 404,
-            mensaje: 'No se encontró un libro con ese id'
-        };
+    if (arrayBooks.length != arrayFiltrado.length){
+        arrayBooks = arrayFiltrado;
+        respuesta= {error: false,codigo: 200, mensaje: 'Libro eliminado', data:arrayBooks};
     }
-    response.send(respuesta);
+    else {
+        respuesta= {error: true, codigo: 404, mensaje: 'No se encontró un libro con ese id', data:arrayBooks};
+    }
+    response.send(respuesta)
 }
 
 function getOneBook(request, response) {
+    
     console.log("end point funciona");
+    
     let respuesta;
     let bookId = parseInt(request.params.id_book);
     let book = arrayBooks.find(book => book.id_book === bookId);
 
     if (book) {
-        respuesta = { error: false, codigo: 200, data: book };
+        respuesta = {error: false, codigo: 200, data: book};
     } else {
-        respuesta = {
-            error: true,
-            codigo: 404,
-            mensaje: 'No se encontró un libro con ese id'
-        };
+        respuesta = {error: true, codigo: 404, mensaje: 'No se encontró un libro con ese id'};
     }
 
     response.send(respuesta);
